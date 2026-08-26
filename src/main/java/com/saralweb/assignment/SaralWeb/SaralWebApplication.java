5c4457075bba04e58dfdb739bcf61d3a29249c3c
AshishSoni <techashishsoni@gmail.com>
2026-08-26 15:47:02 +0530

    This is a initial Commit for SaralWeb Application

---
+++ src/main/java/com/saralweb/assignment/SaralWeb/SaralWebApplication.java
@@ -0,0 +1,13 @@
+package com.saralweb.assignment.SaralWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaralWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaralWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saralweb/assignment/SaralWeb/SaralWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saralweb.assignment.SaralWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaralWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
